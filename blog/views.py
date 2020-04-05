fc44a7e23920da2503f7e0fdb7cd60072ead6de8
YOUR_USERNAME <armankamal2421@gmail.com>
2020-04-05 22:31:00 +0600

    Added RegisterUser Functionality

--- blog/views.py
+++ blog/views.py
@@ -1,40 +1,12 @@
 from django.shortcuts import render
+from .models import *
 from django.http import HttpResponse
-# Create your views here.
 
-
-posts = [
-
-{
-    'author': 'Arman',
-    'title': 'Blog Post 1',
-    'content':  'First Post content',
-    'date_posted': 'August 27,2014'
-},
-
-{
-    'author': 'Arman',
-    'title': 'Blog Post 1',
-    'content':  'First Post content',
-    'date_posted': 'August 27,2014'
-},
-
-{
-    'author': 'Arman',
-    'title': 'Blog Post 1',
-    'content':  'First Post content',
-    'date_posted': 'August 27,2014'
-},
-
-
-]
 
 def home(request):
-    context = {
-        'posts': posts,
-        'title': 'Home'
-    }
-    return render(request, 'blog/home.html',context)
+    posts = Post.objects.all()
+    
+    return render(request, 'blog/home.html',{'posts':posts})
 
 def about(request):
     return render(request,'blog/about.html')
